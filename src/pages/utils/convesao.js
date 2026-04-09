2c2ce49085f44569db62f6e4bd3bdb461df2c4b9
RobertoDias <ra48273551857@acaonsfatima.org.br>
2026-04-09 19:31:15 -0300

    somar e dividir

---
+++ src/pages/utils/convesao.js
@@ -0,0 +1,4 @@
+export default function lerNumber(valor){
+
+    return(Number(valor.replaceAll(",",".")))
+}
--- src/pages/varEstado/index.jsx
+++ src/pages/varEstado/index.jsx
@@ -1,20 +1,31 @@
 import './index.scss'
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import confetti from 'canvas-confetti';
+import lerNumber from '../utils/convesao';
 
 export default function VarEstado() {
     const [contador, setContador] = useState(0);
-    const[tituloS2,setTituloS2]= useState("Oi")
-    const[tituloS3,setTituloS3]= useState("Escolha um item")
-    const[tituloS4,setTituloS4]= useState(false)
-    const[tituloS5,setTituloS5]= useState('oi')
-    const[descricaoS5,setDescricaoS5]=useState('')
+    const [tituloS2, setTituloS2] = useState("Oi")
+    const [tituloS3, setTituloS3] = useState("Escolha um item")
+    const [tituloS4, setTituloS4] = useState(false)
+    const [tituloS5, setTituloS5] = useState('oi')
+    const [descricaoS5, setDescricaoS5] = useState('')
 
+    const [num1S, setNum1S] = useState(0)
+    const [num2S, setNum2S] = useState(0)
+    const [resS, setResS] = useState(0)
+
+    const [num1D,setNum1D]=useState(0)
+    const [num2D,setNum2D]=useState(0)
+    const [resD,setResD]=useState(0)
 
     
 
+
+
+
     const sixseven = new Audio('/assets/audio/67.mp3')
-       
+
 
     useEffect(() => {
         if (contador === 67) {
@@ -23,7 +34,7 @@ export default function VarEstado() {
                 spread: 100,
                 origin: { y: 0.6 }
             });
-    
+
             const audio = new Audio('/assets/audio/67.mp3');
             audio.play();
         }
@@ -51,31 +62,34 @@ export default function VarEstado() {
 
     }
 
-    function alterarTituloS2(e){
+    function alterarTituloS2(e) {
         let novoValor = e.target.value
         setTituloS2(novoValor)
     }
 
-    function alterarTituloS3(e){
+    function alterarTituloS3(e) {
         let novoValor = e.target.value
         setTituloS3(novoValor)
     }
 
-    function alterarTituloS4(e){
+    function alterarTituloS4(e) {
         let novoValor = e.target.checked
         setTituloS4(novoValor)
     }
 
-    function alterarDescricaoS5(e){
-        let novoValor = e.target.value
-        setDescricaoS5(novoValor)
+    function somar() {
+        let soma = lerNumber(num1S) + lerNumber(num2S)
+        setResS(soma)
     }
 
-    
-    function alterarTituloS5(){
-        
-        setTituloS5(descricaoS5)
+    function subtrair(){
+        let divide = lerNumber(num1D) - lerNumber(num2D)
+        setResD(divide)
     }
+
+
+
+
 
     return (
         <div className='pagina-varEstado pagina'>
@@ -115,14 +129,37 @@ export default function VarEstado() {
             </div>
 
             <div className='seçao'>
-                <h1>Programar e lindezinha? {tituloS4 ? "Sim":"Não"}</h1>
-                <input type="checkbox"  checked={tituloS4} onChange={alterarTituloS4}/> ⬅
+                <h1>Programar e lindezinha? {tituloS4 ? "Sim" : "Não"}</h1>
+                <input type="checkbox" checked={tituloS4} onChange={alterarTituloS4} /> ⬅
             </div>
 
             <div className='seçao'>
                 <h1>{tituloS5}</h1>
-               <input type="text"  value={descricaoS5} onChange={alterarDescricaoS5}/>
-               <button onClick={alterarTituloS5}>Selecione</button>
+                <input type="text" value={descricaoS5} onChange={e => setDescricaoS5(e.target.value)} />
+                <button onClick={() => setTituloS5(descricaoS5)}>Selecione</button>
+            </div>
+
+            <div className='seçao'>
+                <h1>Calculadora</h1>
+                <div className='entrada'>
+                    <input type="text" value={num1S} onChange={e => setNum1S(e.target.value)} />
+                    <div>+</div>
+                    <input type="text" value={num2S} onChange={e => setNum2S(e.target.value)} />
+                    <div>=</div>
+                    <div>{resS}</div>
+                    <button onClick={somar}>Somar</button>
+                </div>
+
+                <div className='entrada'>
+                    <input type="text" value={num1D} onChange={e=> setNum1D(e.target.value)} />
+                    <div>-</div>
+                    <input type="text"value={num2D} onChange={e=> setNum2D(e.target.value)} />
+                    <div>=</div>
+                    <div>{resD}</div>
+                    <button onClick={subtrair}>Subtrair</button>
+                </div>
+
+                
             </div>
 
 
